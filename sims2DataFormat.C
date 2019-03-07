5881cc6bcd1fc071a41c5cf8c0d74f068f62c9e8
Jamie Brown <jamie.brown@york.ac.uk>
2019-03-07 17:12:36 +0000

    First commit

---
+++ sims2DataFormat.C
@@ -0,0 +1,265 @@
+#define sims2DataFormat_cxx
+#include "sims2DataFormat.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+
+void sims2DataFormat::Loop()
+{
+//   In a ROOT session, you can do:
+//      root> .L sims2DataFormat.C
+//      root> sims2DataFormat t
+//      root> t.GetEntry(12); // Fill t data members with entry number 12
+//      root> t.Show();       // Show values of entry 12
+//      root> t.Show(16);     // Read and show values of entry 16
+//      root> t.Loop();       // Loop on all entries
+//
+
+//     This is the loop skeleton where:
+//    jentry is the global entry number in the chain
+//    ientry is the entry number in the current Tree
+//  Note that the argument to GetEntry must be:
+//    jentry for TChain::GetEntry
+//    ientry for TTree::GetEntry and TBranch::GetEntry
+//
+//       To read only selected branches, Insert statements like:
+// METHOD1:
+//    fChain->SetBranchStatus("*",0);  // disable all branches
+//    fChain->SetBranchStatus("branchname",1);  // activate branchname
+// METHOD2: replace line
+//    fChain->GetEntry(jentry);       //read all branches
+//by  b_branchname->GetEntry(ientry); //read only this branch
+   if (fChain == 0) return;
+
+   Long64_t nentries = fChain->GetEntriesFast();
+
+// Parameters to replicate experimental configuration
+   Float_t triggerThresh=50; // keV
+   Bool_t errorFlags=0;
+   Float_t fwhm=4.0; //energy resolution of detectors (%)
+   Float_t sigma=fwhm/235.5; // faction rather than %
+
+// My temporary data definations
+   Int_t eventCounter=0;
+   Int_t amID[2]={-1,-1};
+   Int_t pixel[2][2]={{-1,-1},{-1,-1}};
+   Float_t energy[2][2]={{0,0},{0,0}};
+   Int_t pos[2][2]={{0,0},{0,0}};
+   Int_t nTrigPixels[2]={0,0};
+
+// Definitions for new trees for output of exp. data format
+//   Long_t event = -1;
+   Int_t nAMs = 2;
+   Long_t *event_e = new Long_t[nAMs];
+   Int_t *nTrigPixels_e = new Int_t[nAMs];
+   Int_t *AM_e = new Int_t[nAMs];
+   Int_t *GM_e = new Int_t[nAMs];
+   Int_t *nAMsInEvent_e = new Int_t[nAMs];
+   Int_t *pixel_e = new Int_t[nAMs];
+   Float_t *E = new Float_t[nAMs];
+   Float_t *E_neg = new Float_t[nAMs];
+   Int_t *time_e = new Int_t[nAMs];
+   Int_t *triggerFlag_e = new Int_t[nAMs];
+   Int_t *nloop_e = new Int_t[nAMs];
+   Int_t *timeDetect_e = new Int_t[nAMs];
+   Int_t *timeDetectPos_e = new Int_t[nAMs];
+   Float_t *Temp_e = new Float_t[nAMs];
+   Int_t *pos_e = new Int_t[nAMs];
+   Long_t event_eev;
+   Int_t *AM_flag_eev = new Int_t[nAMs];
+   Int_t *cath_flag_eev = new Int_t[nAMs];
+   Int_t nAMsInEvent_eev;
+   Int_t *nTrigPixels_eev = new Int_t[nAMs];
+   
+   Int_t *Nvar2 = new Int_t[nAMs];
+
+   TString rfileOut = "sortedData";
+   TFile *f2 = new TFile(rfileOut + "_Ecal_simulated.root","recreate");
+   // Event tree
+   TTree *tree2_ev = new TTree("tree2_events","Event tree of E calib data");
+   TTree **tree2 = new TTree*[nAMs];
+   tree2_ev->Branch("event",&event_eev,"event/L");
+   tree2_ev->Branch("nAMsInEvent",&nAMsInEvent_eev,"nAMsInEvent/I");
+   tree2_ev->Branch("nTrigPixels",nTrigPixels_eev,Form("nTrigPixels[%d]/I",nAMs));
+   tree2_ev->Branch("AM_flag",AM_flag_eev,Form("AM_flag[%d]/I",nAMs));
+   tree2_ev->Branch("cath_flag",cath_flag_eev,Form("cath_flag[%d]/I",nAMs));
+   // AM trees
+   for (Int_t im = 0; im < nAMs; im++)
+   {
+      tree2[im] = new TTree(Form("tree2_AM%d",im), Form("E calib data tree AM%d",im));
+      tree2[im]->Branch("event",&event_e[im],"event/L");
+      tree2[im]->Branch("nTrigPixels",&nTrigPixels_e[im],"nTrigPixels/I");
+      tree2[im]->Branch("AM",&AM_e[im],"AM/I");
+      tree2[im]->Branch("GM",&GM_e[im],"GM/I");
+      tree2[im]->Branch("nAMsInEvent",&nAMsInEvent_e[im],"nAMsInEvent/I");
+      tree2[im]->Branch("pixel",&pixel_e[im],"pixel/I");
+      tree2[im]->Branch("E",&E[im],"E/F");
+      tree2[im]->Branch("E_neg",&E_neg[im],"E_neg/F");
+      tree2[im]->Branch("pos",&pos_e[im],"pos/I");
+      tree2[im]->Branch("time",&time_e[im],"time/I");
+      tree2[im]->Branch("triggerFlag",&triggerFlag_e[im],"triggerFlag/I");
+      tree2[im]->Branch("nloop",&nloop_e[im],"nloop/I");
+      tree2[im]->Branch("timeDetect",&timeDetect_e[im],"timeDetect/I");
+      tree2[im]->Branch("timeDetectPos",&timeDetectPos_e[im],"timeDetectPos/I");
+      tree2[im]->Branch("Temp",&Temp_e[im],"Temp/F");
+      Nvar2[im] = tree2[im]->GetNbranches();
+   }
+// End of definitions for output file
+
+// Main loop over input file
+   Long64_t nbytes = 0, nb = 0;
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      // if (Cut(ientry) < 0) continue;
+      
+      eventCounter++;
+      
+//      cout << event_eev << " " << nAMsInEvent_eev << " " << nTrigPixels_eev[0] << " " << nTrigPixels_eev[1] << " " << AM_flag_eev[0] << " " << AM_flag_eev[1] << " " << cath_flag_eev[0] << " " << cath_flag_eev[1]  << endl;
+
+
+// Find which AMs and pixels hit
+// Find AMs
+      amID[0]=findAM(Z1_a);
+      amID[1]=findAM(Z2_a);
+      if(amID[0]==-1 || amID[1]==-1) {
+         if(errorFlags==kTRUE) cout << "Error: Gamma ray not in a detector!" << endl;
+         continue;
+      }
+      if(amID[0]==amID[1]) {
+         if(errorFlags==kTRUE) cout << "Error: Same AM hit by both gammas!" << endl;
+         continue;
+      }
+// Find pixels
+      pixel[0][0]=findPixelID(amID[0],X1_a,Y1_a);
+      pixel[0][1]=findPixelID(amID[0],X1_b,Y1_b);
+      pixel[1][0]=findPixelID(amID[1],X2_a,Y2_a);
+      pixel[1][1]=findPixelID(amID[1],X2_b,Y2_b);
+
+//      if(pixel[0][0]==-1 || pixel[0][1]==-1) {
+//         if(errorFlags==kTRUE) cout << "Error: Gamma ray not in a pixel! " << endl;
+//         continue;
+//      }
+
+// Smear energies
+      energy[0][0]=gRandom->Gaus(E1_a,sigma*E1_a);
+      energy[0][1]=gRandom->Gaus(E1_b,sigma*E1_b);
+      energy[1][0]=gRandom->Gaus(E2_a,sigma*E2_a);
+      energy[1][1]=gRandom->Gaus(E2_b,sigma*E2_b);
+
+// Find ordering and number of triggered pixels
+      for(int im=0; im<2; im++) {
+         if(energy[im][0]>energy[im][1] && pixel[im][0]!=-1) {
+            pos[im][0]=0;
+            pos[im][1]=1;
+         } else {
+            pos[im][0]=1;
+            pos[im][1]=0;
+         }
+         if(pixel[im][0]==pixel[im][1]) {
+            energy[im][pos[im][0]]+=energy[im][pos[im][1]];
+            energy[im][pos[im][1]]=0;
+         }
+         if(energy[im][pos[im][1]]>triggerThresh && pixel[im][1]!=-1) {
+            nTrigPixels[im]=2;
+         }
+         else if(energy[im][pos[im][0]]>triggerThresh && pixel[im][0]!=-1) nTrigPixels[im]=1;
+         else nTrigPixels[im]=0;; // skip if no pixels triggered in either AM
+      }
+      if(nTrigPixels[0]==0 && nTrigPixels[1]==0) continue;
+//      if(pixel[0][0]==pixel[0][1]) {
+////         cout << "Warning: Same pixel hit twice! " << endl;
+//         energy[0][0]+=energy[0][1];
+//         pixel[0][1]=-1;
+////         continue;
+//      }
+      
+//      cout << amID[0] << " " << pixel[0][0] << " " << pixel[0][1] << " " << energy[0][0] << " " << energy[0][1] << " "
+//         << amID[1] << " " << pixel[1][0] << " " << pixel[1][1] << " " << energy[1][0] << " " << energy[1][1] << endl;
+      
+// Fill data for AM trees
+      for(Int_t im = 0; im < nAMs; im++) {
+         event_e[amID[im]]=eventCounter-1;
+         nTrigPixels_e[amID[im]]=nTrigPixels[im];
+//         nTrigPixels_e[amID[im]]=nTrigPixels[im];
+         AM_e[amID[im]]=amID[im];
+         GM_e[amID[im]]=1;
+         nAMsInEvent_e[amID[im]]=2;
+         pixel_e[amID[im]]=pixel[im][pos[im][0]];
+         E[amID[im]]=energy[im][pos[im][0]];
+//         if(E[amID[im]]<triggerThresh) cout << "Error! Error! " << event_e[amID[im]] << " " << amID[im] << " " << nTrigPixels_e[amID[im]] << endl;
+         E_neg[amID[im]]=0;
+         time_e[amID[im]]=0;
+         triggerFlag_e[amID[im]]=0;
+         nloop_e[amID[im]]=0;
+         timeDetect_e[amID[im]]=0;
+         timeDetectPos_e[amID[im]]=0;
+         Temp_e[amID[im]]=0;
+         pos_e[amID[im]]=0;
+         if(nTrigPixels_e[amID[im]]!=0) tree2[amID[im]]->Fill();
+         // if two pixel hits, every thing same except pixel and energy
+         if(nTrigPixels_e[amID[im]]==2) {
+            pixel_e[amID[im]]=pixel[im][pos[im][1]];
+            E[amID[im]]=energy[im][pos[im][1]];
+            pos_e[amID[im]]=1;
+            tree2[amID[im]]->Fill();
+         }
+      }
+      
+      // Fill data for event tree
+      event_eev=eventCounter-1;
+      nAMsInEvent_eev=2;
+      for(Int_t im = 0; im < nAMs; im++) {
+         nTrigPixels_eev[amID[im]]=nTrigPixels_e[amID[im]];
+         AM_flag_eev[im]=1;
+         cath_flag_eev[im]=0;
+      }
+      tree2_ev->Fill();
+      
+   }
+// End of loop over input file
+   
+// Write trees and close files
+   f2->cd();
+   tree2_ev->Write();
+   for (Int_t im = 0; im < nAMs; im++)
+   {
+      tree2[im]->Write();
+   }
+   f2->Close();
+   
+} // End of sims2DataFormat::Loop()
+
+Int_t findAM(Float_t z)
+{
+   Int_t am=-1;
+   if(z>=80) am=0;
+   else if(z<=-80) am=1;
+   
+   return am;
+}
+
+Int_t findPixelID(Int_t am, Float_t x, Float_t y)
+{
+   Int_t pixelID=-1;
+   Int_t row=-1, col=-1;
+   
+   // AM 1 if rotated 180 degrees so invert x
+   if(am==1) x=-x;
+   
+   // added 100um to extremes
+   Float_t pixEdges[12]={-4.5,-3.6,-2.8,-2.0,-1.2,-0.4,0.4,1.2,2.0,2.8,3.6,4.5};
+   for(Int_t i=0; i<12; i++) {
+      if(x>pixEdges[i] && x<=pixEdges[i+1]) col=i+1;
+      if(y>pixEdges[i] && y<=pixEdges[i+1]) row=11-i;
+   }
+//   if(row!=-1 && col!=-1) pixelID=col+(row-1)*11;
+   if(row!=-1 && col!=-1) pixelID=col+(row-1)*44;
+   
+//   if(pixelID!=-1){
+   if(am==0) return pixelID;
+   else if(am==1 && pixelID!=-1) return pixelID+22;
+   else return pixelID;
+}
+
---
+++ sims2DataFormat.h
@@ -0,0 +1,193 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Wed Mar  6 11:01:20 2019 by ROOT version 6.12/06
+// from TTree tree/lab setup entangled sim
+// found on file: 2billion.root
+//////////////////////////////////////////////////////////
+
+#ifndef sims2DataFormat_h
+#define sims2DataFormat_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TTree.h>
+
+// Header file for the classes stored in the TTree if any.
+
+class sims2DataFormat {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+   // Declaration of leaf types
+   Float_t         theta1;
+   Float_t         phi1;
+   Float_t         E1_a;
+   Float_t         X1_a;
+   Float_t         Y1_a;
+   Float_t         Z1_a;
+   Float_t         E1_b;
+   Float_t         X1_b;
+   Float_t         Y1_b;
+   Float_t         Z1_b;
+   Float_t         theta2;
+   Float_t         phi2;
+   Float_t         E2_a;
+   Float_t         X2_a;
+   Float_t         Y2_a;
+   Float_t         Z2_a;
+   Float_t         E2_b;
+   Float_t         X2_b;
+   Float_t         Y2_b;
+   Float_t         Z2_b;
+
+   // List of branches
+   TBranch        *b_theta1;   //!
+   TBranch        *b_phi1;   //!
+   TBranch        *b_E1_a;   //!
+   TBranch        *b_X1_a;   //!
+   TBranch        *b_Y1_a;   //!
+   TBranch        *b_Z1_a;   //!
+   TBranch        *b_E1_b;   //!
+   TBranch        *b_X1_b;   //!
+   TBranch        *b_Y1_b;   //!
+   TBranch        *b_Z1_b;   //!
+   TBranch        *b_theta2;   //!
+   TBranch        *b_phi2;   //!
+   TBranch        *b_E2_a;   //!
+   TBranch        *b_X2_a;   //!
+   TBranch        *b_Y2_a;   //!
+   TBranch        *b_Z2_a;   //!
+   TBranch        *b_E2_b;   //!
+   TBranch        *b_X2_b;   //!
+   TBranch        *b_Y2_b;   //!
+   TBranch        *b_Z2_b;   //!
+
+   sims2DataFormat(TTree *tree=0);
+   virtual ~sims2DataFormat();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+   
+};
+
+#endif
+
+#ifdef sims2DataFormat_cxx
+sims2DataFormat::sims2DataFormat(TTree *tree) : fChain(0) 
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0) {
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("2billion.root");
+      if (!f || !f->IsOpen()) {
+         f = new TFile("2billion.root");
+      }
+      f->GetObject("tree",tree);
+
+   }
+   Init(tree);
+}
+
+sims2DataFormat::~sims2DataFormat()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t sims2DataFormat::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t sims2DataFormat::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void sims2DataFormat::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("theta1", &theta1, &b_theta1);
+   fChain->SetBranchAddress("phi1", &phi1, &b_phi1);
+   fChain->SetBranchAddress("E1_a", &E1_a, &b_E1_a);
+   fChain->SetBranchAddress("X1_a", &X1_a, &b_X1_a);
+   fChain->SetBranchAddress("Y1_a", &Y1_a, &b_Y1_a);
+   fChain->SetBranchAddress("Z1_a", &Z1_a, &b_Z1_a);
+   fChain->SetBranchAddress("E1_b", &E1_b, &b_E1_b);
+   fChain->SetBranchAddress("X1_b", &X1_b, &b_X1_b);
+   fChain->SetBranchAddress("Y1_b", &Y1_b, &b_Y1_b);
+   fChain->SetBranchAddress("Z1_b", &Z1_b, &b_Z1_b);
+   fChain->SetBranchAddress("theta2", &theta2, &b_theta2);
+   fChain->SetBranchAddress("phi2", &phi2, &b_phi2);
+   fChain->SetBranchAddress("E2_a", &E2_a, &b_E2_a);
+   fChain->SetBranchAddress("X2_a", &X2_a, &b_X2_a);
+   fChain->SetBranchAddress("Y2_a", &Y2_a, &b_Y2_a);
+   fChain->SetBranchAddress("Z2_a", &Z2_a, &b_Z2_a);
+   fChain->SetBranchAddress("E2_b", &E2_b, &b_E2_b);
+   fChain->SetBranchAddress("X2_b", &X2_b, &b_X2_b);
+   fChain->SetBranchAddress("Y2_b", &Y2_b, &b_Y2_b);
+   fChain->SetBranchAddress("Z2_b", &Z2_b, &b_Z2_b);
+   Notify();
+}
+
+Bool_t sims2DataFormat::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void sims2DataFormat::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t sims2DataFormat::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+
+Int_t findAM(Float_t z);
+
+Int_t findPixelID(Int_t am, Float_t x, Float_t y);
+
+#endif // #ifdef sims2DataFormat_cxx
