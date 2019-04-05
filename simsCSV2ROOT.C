e71229ed95b5202383b52d635c0e78f41fc0edd4
Jamie Brown <jamie.brown@york.ac.uk>
2019-04-05 14:00:12 +0100

    Added annihil_N variable to simsCSV2ROOT.C

--- simsCSV2ROOT.C
+++ simsCSV2ROOT.C
@@ -2,7 +2,7 @@
    TFile *f=new TFile("2billion_1.root","recreate");
    TTree* t=new TTree("tree","lab setup entangled sim");
    Long64_t nlines=t->ReadFile("2billion (1).csv",
-   "theta1:phi1:E1_a:X1_a:Y1_a:Z1_a:E1_b:X1_b:Y1_b:Z1_b:theta2:phi2:E2_a:X2_a:Y2_a:Z2_a:E2_b:X2_b:Y2_b:Z2_b");
+   "annihil_N:theta1:phi1:E1_a:X1_a:Y1_a:Z1_a:E1_b:X1_b:Y1_b:Z1_b:theta2:phi2:E2_a:X2_a:Y2_a:Z2_a:E2_b:X2_b:Y2_b:Z2_b");
    cout << nlines << " lines found in file" << endl;
    t->Write();
    f->Close();
